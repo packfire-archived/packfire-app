9d6d7de8ccf6a28fe1b26a2856408401cfa2a9d6
Sam-Mauris Yong <mauris@hotmail.sg>
2012-05-09 23:59:45 +0800

    added copyright and removed error suppressor
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
  * Packfire Application Front Controller 
  * 
  * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @copyright Copyright (c) 2010-2012, Sam-Mauris Yong
  * @license http://www.opensource.org/licenses/bsd-license New BSD License
  * @since 1.0-sofia
  * @internal
@@ -22,7 +23,7 @@ define('__PACKFIRE_PATH__', '../packfire');
 define('__ENVIRONMENT__' , '');
 
 // include the main Packfire class
-$ok = @include(__PACKFIRE_PATH__ . '/Packfire.php');
+$ok = include(__PACKFIRE_PATH__ . '/Packfire.php');
 if($ok){
     pload('app.Application');
     // IMMA FIRIN' MA LAZOR
