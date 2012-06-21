abc6ba16ad12426fc465d62e7f3c3cc3d75ebb6b
Sam-Mauris Yong <mauris@hotmail.sg>
2012-06-21 16:00:28 +0800

    removed overriding method in AppSecurityModule
    Signed-off-by: Sam-Mauris Yong <mauris@hotmail.sg>

--- pack/app/AppSecurityModule.php
+++ pack/app/AppSecurityModule.php
@@ -2,7 +2,7 @@
 pload('packfire.security.pSecurityModule');
 
 /**
- * AppSecurityModule
+ * AppSecurityModule class
  * 
  * The application's security module
  * Configure by overriding the appropriate methods.
@@ -26,10 +26,5 @@ class AppSecurityModule extends pSecurityModule {
     public function deauthenticate() {
         parent::deauthenticate();
     }
-
-    public function identity($newIdentity = null) {
-        return parent::identity($newIdentity);
-    }
-
     
 }
