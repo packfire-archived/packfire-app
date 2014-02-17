19bada63f37bf20f44841e005c69f3004d89d67f
Sam-Mauris Yong <mauris@hotmail.sg>
2014-02-17 22:23:03 +0800

    updated front controller file

--- web/index.php
+++ web/index.php
@@ -1,19 +1,7 @@
 <?php
 
-use Packfire\Application\Http\Application as HttpApplication;
-
-/**
- * Packfire Application Front Controller for HTTP interface
- * 
- * @author Sam-Mauris Yong / mauris@hotmail.sg
- * @copyright Copyright (c) 2010-2012, Sam-Mauris Yong
- * @license http://www.opensource.org/licenses/bsd-license New BSD License
- * @since 1.0-sofia
- * @internal
- * @ignore
- */
-
-$packfire = require(__DIR__ . '/../app/bootstrap.php');
-if($packfire){
+$app_dir = __DIR__ . '/..';
+$packfire = require($app_dir . '/app/bootstrap.php');
+if ($packfire) {
     $packfire->fire(new HttpApplication());
 }
